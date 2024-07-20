b152e0d3ba9d05570f3bf0b68db859e50ae4805a
Gerek Jordan <gejordan@csumb.edu>
2024-07-20 15:28:32 -0700

    Created Monster class

---
+++ src/main/java/Monster.java
@@ -0,0 +1,9 @@
+/**
+ * @author Gerek G. Jordan
+ * CST 338 Software Design Summer 24
+ * Due Date: July 23rd, 2024
+ * https://github.com/CST-338/strategy-pattern-gjordn.git
+ *
+ */
+public class Monster {
+}
