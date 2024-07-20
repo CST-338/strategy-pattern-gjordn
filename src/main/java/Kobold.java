f6a8a562ce1070d557ca2a359969a4b7e10c6617
Gerek Jordan <gejordan@csumb.edu>
2024-07-20 15:55:27 -0700

    Finished Kobold class

--- src/main/java/Kobold.java
+++ src/main/java/Kobold.java
@@ -8,14 +8,13 @@
 
 import java.util.HashMap;
 
-public class Imp extends Monster {
-    public Imp(Integer maxHP, Integer xp, HashMap<String, Integer> items) {
+public class Kobold extends Monster{
+    public Kobold(Integer maxHP, Integer xp, HashMap<String, Integer> items) {
         super(maxHP, xp, items);
     }
 
     @Override
     public String toString() {
-        return "Imp has : " + super.toString();
+        return "Kobold has : " + super.toString();
     }
-
 }
